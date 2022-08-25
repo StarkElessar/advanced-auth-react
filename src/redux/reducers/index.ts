23a957c7ed5b42b16e755ea38fa961baff16a827
Stark <serj.elessar@gmail.com>
2022-08-25 22:14:20 +0300

    create and export type RootState

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux"
 import userReducer from "./userReducer"
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
   user: userReducer
 })
 
-export default rootReducer
+export type RootState = ReturnType<typeof rootReducer>

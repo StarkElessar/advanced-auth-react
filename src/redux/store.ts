a2be638892912af4fd663335a2dba199a50767db
Stark <serj.elessar@gmail.com>
2022-08-25 22:13:57 +0300

    moved type

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux"
 import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+import { rootReducer } from './reducers'
 
 const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
-//! не забудь потом переместить это отдельно в типы:
-export type AppDispatch = typeof store.dispatch

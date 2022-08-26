dccb9285d200551360920b062dc8bd4520cecebf
Stark <serj.elessar@gmail.com>
2022-08-26 09:37:08 +0300

    feature: useActions hook

---
+++ src/hooks/useActions.ts
@@ -0,0 +1,9 @@
+import { useDispatch } from "react-redux"
+import { bindActionCreators } from "redux"
+import * as UserActionCreators from '../redux/actions/userActions'
+
+export const useActions = () => {
+  const dispatch = useDispatch()
+
+  return bindActionCreators(UserActionCreators, dispatch)
+}

4c00d7974b97cfc3236c23d967ae07fb782ae32f
Stark <serj.elessar@gmail.com>
2022-08-27 11:56:50 +0300

    fix: changed export

--- src/services/AxiosService.ts
+++ src/services/AxiosService.ts
@@ -1,8 +1,16 @@
 import axios from 'axios'
+import AuthService from './AuthService'
 
-export const API_URL: string = `http://localhost:5000/api`
+export const HEROKU: string = `https://advanced-auth-back.herokuapp.com/api`
+export const LOCAL: string = `http://localhost:5000/api`
+export const API_URL = HEROKU
 
-const $api = axios.create({
+export const $api = axios.create({
+  withCredentials: true,
+  baseURL: API_URL,
+})
+
+export const $apiRefresh = axios.create({
   withCredentials: true,
   baseURL: API_URL,
 })
@@ -13,4 +21,24 @@ $api.interceptors.request.use((config) => {
   return config
 })
 
-export default $api
+$api.interceptors.response.use((config) => {
+    return config
+  }, async (error) => {
+    const originalRequest = error.config
+
+    if (error.response.status === 403 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true
+
+      try {
+        const response = await AuthService.refresh()
+        localStorage.setItem('token', response.data.accessToken)
+
+        return $api.request(originalRequest)
+      } catch (error) {
+        console.log('Пользователь не авторизован')
+      }
+    }
+  
+    throw error
+  }
+)

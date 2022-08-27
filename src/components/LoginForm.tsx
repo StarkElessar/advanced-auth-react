12bc4fcea9a6d3ac70ea7920f51ad0790ac532cf
Stark <serj.elessar@gmail.com>
2022-08-27 16:47:12 +0300

    fix: edited login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,47 +1,126 @@
-import { FC, ChangeEvent, useState } from 'react'
+import { FC, ChangeEvent, useState, useCallback, useEffect, memo } from 'react'
+import { Button, Checkbox, Form, Input } from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import { useActions } from '../hooks/useActions'
+import { useTypedSelector } from '../hooks/useTypedSelector'
+import { ADMIN_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE } from '../utils/consts'
 
 const LoginForm: FC = () => {
-  const { userLogin, userRegister } = useActions()
+  const navigate = useNavigate()
+  const { userLogin } = useActions()
+  const { statusLogin, error } = useTypedSelector(({ user }) => user)
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [messageStatus, setMessageStatus] = useState<string>('')
 
   const onSetEmail = (event: ChangeEvent<HTMLInputElement>) =>
     setEmail(event.target.value)
+
   const onSetPassword = (event: ChangeEvent<HTMLInputElement>) =>
     setPassword(event.target.value)
 
-  const onLogin = () => userLogin(email, password)
-  const onRegister = () => userRegister(email, password)
+  const onLogin = () => {
+    userLogin(email, password)
+    
+    if (statusLogin === 'success') {
+      navigate(ADMIN_ROUTE)
+    } else if (statusLogin === 'loading') {
+      setMessageStatus('Процесс авторизации..')
+    } else if (statusLogin === 'error') {
+      navigate(LOGIN_ROUTE)
+      setMessageStatus('Ошибка авторизации, повторите снова')
+      setEmail('')
+      setPassword('')
+    }
+  }
+
+  console.log(messageStatus)
 
   return (
-    <div className="login">
-      <input
-        className="login__input"
-        value={email}
-        onChange={onSetEmail}
-        type="email"
-        placeholder="Email"
-      />
-      <input
-        className="login__input"
-        value={password}
-        onChange={onSetPassword}
-        type="password"
-        placeholder="Password"
-      />
-      <button
-        disabled={!email || !password}
-        className="login__btn"
-        onClick={onLogin}
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+    >
+      <h1 style={{ textAlign: 'center' }}>Войти в кабинет</h1>
+      <div style={{ textAlign: 'center', marginBottom: 20, color: '#ff3636' }}>
+        {messageStatus}
+      </div>
+      {statusLogin === 'loading' && (
+        <div style={{
+          textAlign: 'center',
+          marginBottom: 20,
+          color: '#ff3636'
+          }}
+        >
+          Процесс авторизации..
+        </div>
+      )}
+      {statusLogin === 'success' && (
+        <div style={{
+          textAlign: 'center',
+          marginBottom: 20,
+          color: '#ff3636'
+          }}
+        >
+          Вы успешно авторизовались!
+        </div>
+      )}
+
+      <Form.Item
+        name="useremail"
+        rules={[
+          {
+            required: true,
+            message: 'Пожалуйста введите ваш действующий email!',
+          },
+        ]}
       >
-        Войти
-      </button>
-      <button className="login__btn" onClick={onRegister}>
-        Зарегистрироваться
-      </button>
-    </div>
+        <Input
+          value={email}
+          onChange={onSetEmail}
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="E-mail"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Пожалуйста введите ваш пароль!' }]}
+      >
+        <Input
+          value={password}
+          onChange={onSetPassword}
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Link className="login-form-forgot" to="#">
+          Забыли пароль
+        </Link>
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          onClick={onLogin}
+          type="primary"
+          htmlType="submit"
+          className="login-form-button"
+          style={{ marginRight: 10 }}
+        >
+          Войти
+        </Button>
+        или <Link to={REGISTER_ROUTE}>Создать аккаунт</Link>
+      </Form.Item>
+    </Form>
   )
 }
 
-export default LoginForm
+export default memo(LoginForm)

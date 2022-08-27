53821a4673e9e1b55004a36cadfe06d63360ace8
Stark <serj.elessar@gmail.com>
2022-08-27 11:59:03 +0300

    fix: edit fetchUsers action and checkAuth

--- src/redux/actions/userActions.ts
+++ src/redux/actions/userActions.ts
@@ -1,19 +1,14 @@
-import axios from 'axios'
 import { Dispatch } from 'redux'
 
 import AuthService from '../../services/AuthService'
-import { AuthResponse } from '../../types/AuthResponse'
-import { API_URL } from '../../services/AxiosService'
+import UserService from '../../services/UserService'
 import { UserAction, UserActionTypes } from '../../types/user'
 
 export const fetchUsers = () => async (dispatch: Dispatch<UserAction>) => {
   try {
     dispatch({ type: UserActionTypes.FETCH_USERS })
 
-    const response = await axios.get(`${API_URL}/users`, {
-      withCredentials: true,
-    })
-    console.log(response)
+    const response = await UserService.fetchUsers()
 
     dispatch({
       type: UserActionTypes.FETCH_USERS_SUCCESS,
@@ -73,14 +68,16 @@ export const userLogout = () => async (dispatch: Dispatch<UserAction>) => {
 }
 
 export const checkAuth = () => async (dispatch: Dispatch<UserAction>) => {
+  dispatch({ type: UserActionTypes.SET_LOADING, payload: true })
   try {
-    const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
-      withCredentials: true,
-    })
+    const response = await AuthService.refresh()
+    console.log(response)
 
     localStorage.setItem('token', response.data.accessToken)
     dispatch({ type: UserActionTypes.SET_AUTH, payload: response.data.user })
   } catch (error: any) {
     console.log(error.response?.data?.message)
+  } finally {
+    dispatch({ type: UserActionTypes.SET_LOADING, payload: false })
   }
 }
